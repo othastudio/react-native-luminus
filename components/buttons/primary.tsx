83e25cf99eeeb986d1abc41cc7d6f7e4c01d2675
Othmane N <otmannj56@gmail.com>
2023-05-22 14:59:58 +0100

    working to add luminus UI

---
+++ components/buttons/primary.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Primary() {
+  return (
+    <View>
+      <Text>Primary button</Text>
+    </View>
+  )
+}
